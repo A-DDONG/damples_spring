daf6f36fbc3d33f49cafc92781bf5201a56f7179
A-DDONG <ssc5924@gmail.com>
2023-07-11 16:42:34 +0900

    데이터베이스해보자

--- Damples/src/main/java/com/damples/damdam/HomeController.java
+++ Damples/src/main/java/com/damples/damdam/HomeController.java
@@ -5,11 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.damples.damdam.dao.TourDao;
+
+	
 @Controller
 public class HomeController {
 	
+	private final TourDao tourDao;
+	
+	public HomeController(TourDao tourDao) {
+		this.tourDao = tourDao;
+	}
+
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
+		
+		
 		
 		return "home";
 	}
---
+++ Damples/src/main/java/com/damples/damdam/dao/TourDao.java
@@ -0,0 +1,14 @@
+package com.damples.damdam.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.damples.damdam.models.TourDto;
+
+public interface TourDao {
+	
+	@Select(" SELECT * FROM tbl_tour ")
+	public List<TourDto> selectAll();
+
+}

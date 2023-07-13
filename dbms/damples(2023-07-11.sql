3c5ecfa2cd6e9810358c2bf289156efe367bb945
A-DDONG <ssc5924@gmail.com>
2023-07-13 16:41:55 +0900

    db연습

--- dbms/damples(2023-07-11.sql
+++ dbms/damples(2023-07-11.sql
@@ -3,7 +3,7 @@ DROP TABLE tbl_tour;
 USE tourDB;
 CREATE TABLE tbl_tour (
 	tour_code VARCHAR(5) PRIMARY KEY,
-    tour_category VARCHAR(5),
+    tour_category VARCHAR(20),
     tour_name VARCHAR(50),
     tour_addr VARCHAR(125),
     tour_time VARCHAR(200),
@@ -11,7 +11,7 @@ CREATE TABLE tbl_tour (
     tour_tel VARCHAR(15),
     tour_notic VARCHAR(200),
     tour_page VARCHAR(100),
-    tour_fee VARCHAR(100),
+    tour_fee VARCHAR(200),
     tour_info VARCHAR(300),
     tour_imgcode VARCHAR(5)
 );
